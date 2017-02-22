0caf2486ae07b0872af3f732710e759ec3d4dc19
Karen Badoyan <badoyank@mail.ru>
2017-02-22 20:33:22 +0400

    Second commit

--- app/src/main/java/am/karen/sendparse/MainActivity.java
+++ app/src/main/java/am/karen/sendparse/MainActivity.java
@@ -49,9 +49,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
 
 
 
